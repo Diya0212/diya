b372eb53ad3fc6cf18b9d3716fc7a7015c21c7bb
diya <diyabansal02@gmail.com>
2022-04-27 14:04:45 +0530

    First Commit

---
+++ P1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//using namespace std;
+
+int main(){
+
+    //cout<<"hello world"<<endl;
+    //cout<</n"i am diya";
+    //cout<<"my roll number is 0473"<<endl;
+
+    std::cout<<"Hello world"<<std::endl;
+    std::cout<<"I am diya"<<std::endl;
+    std::cout<<"My roll number is 0473"<<std::endl;
+}
